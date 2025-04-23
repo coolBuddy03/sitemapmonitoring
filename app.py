da5acc585ea1b7d0a05f49d126531b2acce13f2d
coolBuddy03 <33701077+coolBuddy03@users.noreply.github.com>
2025-04-23 22:12:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -73,4 +73,4 @@ def download_zip():
         return f'Error creating zip file: {str(e)}', 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
